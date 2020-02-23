f79e33fca8282fa369c5d1ea116f99f6bdf473da
Jash Shah <jash.shah@live.com>
2020-02-23 13:21:42 +0530

    Changes 200223

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,12 @@ import { sections } from '../components/section';
 import { uses } from '../components/uses';
 import "bootstrap";
 
+const leg = document.querySelectorAll("legend");
+window.addEventListener('scroll', function(e) {
+  console.log(leg);
+});
+
+
 uses();
 sections();
 details();
